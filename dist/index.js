c2c26bff5b99a0c0c3f5269429e30eda31834f06
bookmin80 <bookmin80@gmail.com>
2024-01-13 21:55:38 +0100

    First commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function excercise1() {
+    let greeting;
+    greeting = "Hello World";
+    greeting += '!!!';
+    console.log(greeting);
+}
+excercise1();
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+function excercise1() {
+  let greeting: string;
+  greeting = "Hello World";
+  greeting += '!!!';
+  console.log(greeting);
+}
+excercise1();
